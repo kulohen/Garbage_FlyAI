018942acc5ebcaecf8a6fd38aeab928e711e68fd
kulohen <291255700@qq.com>
2019-07-14 02:13:44 +0800

    v2 ResNet50 原生

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ Created on Mon Oct 30 19:44:02 2017
 
 import argparse
 
-import keras
+from keras.applications import ResNet50
 from flyai.dataset import Dataset
 from keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense, Activation, MaxPooling2D
 from keras.models import Sequential
 from model import Model
 from path import MODEL_PATH
+from keras.callbacks import EarlyStopping, TensorBoard,ModelCheckpoint
+
 
 '''
 Tensorflow模版项目下载： https://www.flyai.com/python/tensorflow_template.zip
@@ -27,8 +29,8 @@ Keras模版项目下载： https://www.flyai.com/python/keras_template.zip
 项目的超参
 '''
 parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--EPOCHS", default=10, type=int, help="train epochs")
-parser.add_argument("-b", "--BATCH", default=32, type=int, help="batch size")
+parser.add_argument("-e", "--EPOCHS", default=100, type=int, help="train epochs")
+parser.add_argument("-b", "--BATCH", default=100, type=int, help="batch size")
 args = parser.parse_args()
 
 '''
@@ -41,26 +43,8 @@ model = Model(dataset)
 '''
 实现自己的网络机构
 '''
-sqeue = Sequential()
-
-# 第一个卷积层，32个卷积核，大小５x5，卷积模式SAME,激活函数relu,输入张量的大小
-sqeue.add(Conv2D(filters=32, kernel_size=(5, 5), padding='Same', activation='relu',
-                 input_shape=(224, 224, 3)))
-sqeue.add(Conv2D(filters=32, kernel_size=(5, 5), padding='Same', activation='relu'))
-# 池化层,池化核大小２x2
-sqeue.add(MaxPool2D(pool_size=(2, 2)))
-sqeue.add(Dropout(0.25))
-sqeue.add(Conv2D(filters=64, kernel_size=(3, 3), padding='Same', activation='relu'))
-sqeue.add(Conv2D(filters=64, kernel_size=(3, 3), padding='Same', activation='relu'))
-sqeue.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2)))
-sqeue.add(Dropout(0.25))
-# 全连接层,展开操作，
-sqeue.add(Flatten())
-# 添加隐藏层神经元的数量和激活函数
-sqeue.add(Dense(256, activation='relu'))
-sqeue.add(Dropout(0.25))
-# 输出层
-sqeue.add(Dense(6, activation='softmax'))
+num_classes = 6
+sqeue = ResNet50( weights=None, input_shape=(224, 224, 3), classes= num_classes, include_top=True)
 
 # 输出模型的整体信息
 sqeue.summary()
@@ -73,19 +57,16 @@ sqeue.compile(loss='categorical_crossentropy',
 dataset.get_step() 获取数据的总迭代次数
 
 '''
+# checkpoint = ModelCheckpoint( monitor='val_acc', mode='auto', save_best_only=True)
 best_score = 0
 for step in range(dataset.get_step()):
     x_train, y_train = dataset.next_train_batch()
     x_val, y_val = dataset.next_validation_batch()
-    history = sqeue.fit(x_train, y_train,
+    history = sqeue.fit(x_train, y_train, validation_data=(x_val , y_val),
                         batch_size=args.BATCH,
+                        # callback = checkpoint,
+                        epochs =args.EPOCHS,
                         verbose=1)
-    score = sqeue.evaluate(x_val, y_val, verbose=0)
-    if score[1] > best_score:
-        best_score = score[1]
-        '''
-        保存模型
-        '''
-        model.save_model(sqeue, MODEL_PATH, overwrite=True)
-        print("step %d, best accuracy %g" % (step, best_score))
+
     print(str(step + 1) + "/" + str(dataset.get_step()))
+model.save_model(sqeue, MODEL_PATH, overwrite=True)
