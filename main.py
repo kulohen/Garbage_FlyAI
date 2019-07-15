ed049a55e9ed20b0e055a3c38a4aefc90c1d7878
kulohen <291255700@qq.com>
2019-07-15 23:00:01 +0800

    update5 validate merge batchnormalize earlystopping by loss

--- main.py
+++ main.py
@@ -7,30 +7,31 @@ Created on Mon Oct 30 19:44:02 2017
 update4 尝试新增一些东西
 validata_data 取代 split
 512 × 384 ，输入图片再改改
+
+update5
+validate merge
+batchnormalize
+earlystopping by loss
 """
 
 import argparse
 
 from keras.applications import ResNet50,VGG16
 from flyai.dataset import Dataset
-from keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense, Activation, MaxPooling2D,ZeroPadding2D
+from keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense, Activation, MaxPooling2D,ZeroPadding2D,BatchNormalization
 from keras.models import Sequential
 from model import Model
 from path import MODEL_PATH
 from keras.callbacks import EarlyStopping, TensorBoard,ModelCheckpoint
-from keras.optimizers import SGD
-
-'''
-update2
-'''
-
+from keras.optimizers import SGD,adam
+import numpy as np
 
 '''
 项目的超参
 '''
 parser = argparse.ArgumentParser()
 parser.add_argument("-e", "--EPOCHS", default=100, type=int, help="train epochs")
-parser.add_argument("-b", "--BATCH", default=16, type=int, help="batch size")
+parser.add_argument("-b", "--BATCH", default=32, type=int, help="batch size")
 args = parser.parse_args()
 
 '''
@@ -55,7 +56,7 @@ print('dataset.get_all_validation_data():',dataset.get_validation_length())
 '''
 num_classes = 6
 sqeue = Sequential()
-sqeue.add(Conv2D(32, (3, 3), activation='relu', padding='same',input_shape=(384, 512, 3)))
+sqeue.add(Conv2D(32, (3, 3), activation='relu', padding='same',input_shape=(224, 224, 3)))
 sqeue.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
 sqeue.add(MaxPooling2D(pool_size=(2, 2)))
 # sqeue.add(Dropout(0.25))
@@ -75,8 +76,10 @@ sqeue.add(MaxPooling2D(pool_size=(2, 2)))
 
 sqeue.add(Flatten())
 sqeue.add(Dense(1024, activation='relu'))
+sqeue.add(BatchNormalization())
 sqeue.add(Dropout(0.5))
 sqeue.add(Dense(1024, activation='relu'))
+sqeue.add(BatchNormalization())
 sqeue.add(Dropout(0.5))
 sqeue.add(Dense(num_classes, activation='softmax'))
 
@@ -87,20 +90,23 @@ sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
 sqeue.summary()
 
 sqeue.compile(loss='categorical_crossentropy',
-              optimizer='sgd',
+              optimizer='adam',
               metrics=['accuracy'])
 
 
 # checkpoint = ModelCheckpoint( monitor='val_acc', mode='auto', save_best_only=True)
 early_stopping = EarlyStopping(monitor='loss', patience=10 ,verbose=1)
 best_score = 0
-# for step in range(dataset.get_step()):
-history = sqeue.fit(x=x_train, y=y_train,
+
+x_train_and_x_val = np.concatenate((x_train, x_val),axis=0)
+y_train_and_y_val= np.concatenate((y_train , y_val),axis=0)
+print('x_train_and_x_val.size :', x_train_and_x_val.shape)
+history = sqeue.fit(x=x_train_and_x_val, y=y_train_and_y_val,
                     validation_data=(x_val , y_val),
                     # validation_split=0.2,
                     shuffle=True,
                     batch_size=args.BATCH,
-                    # callbacks = [early_stopping],
+                    callbacks = [early_stopping],
                     epochs =args.EPOCHS,
                     verbose=2)
 
